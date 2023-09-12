fa5f26b3c7fd8aefa872cd447a22dd3a16926d30
Danny Moulier <mr.moulier@gmail.com>
2023-09-12 20:52:26 +0300

    Added basic funcionality

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # odin-calculator
+
+
+1. a list of numbers cliked
+2. whewn an operator clicked, 
+the numbers list reduced to a whole integer and choice for num2 begins
+3. when click =, numbers evaluated
--- script.js
+++ script.js
@@ -14,20 +14,60 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
+function modulo(num1, num2) {
+    return num1 % num2;
+}
 
 let num1;
 let num2;
 let operator;
 
-
 function operate(operator, num1, num2) {
-    if (operator === '+') {
-        return add(num1, num2)
-    } else if (operator === '-') {
-        return subtract(num1, num2)
-    } else if (operator === '*') {
-        return multiply(num1, num2)
-    } else if (operator === '/') {
-        return divide(num1, num2)
+    if (operator === "+") {
+        return add(num1, num2);
+    } else if (operator === "-") {
+        return subtract(num1, num2);
+    } else if (operator === "*") {
+        return multiply(num1, num2);
+    } else if (operator === "/") {
+        return divide(num1, num2);
     }
 }
+
+const numberButtons = document.querySelectorAll(".number");
+const operatorButtons = document.querySelectorAll(".operator");
+const equalsButton = document.querySelector("#equals");
+
+let num1Arr = [];
+
+numberButtons.forEach((numberButton) => {
+    numberButton.addEventListener("click", function () {
+        num1Arr.push(numberButton.innerText);
+    });
+});
+
+operatorButtons.forEach((operatorButton) => {
+    operatorButton.addEventListener("click", function () {
+        if (num1Arr) {
+            num1 = parseInt(num1Arr.join(""));
+            operator = operatorButton.innerText;
+        }
+    });
+});
+
+let num2Arr = [];
+
+numberButtons.forEach((numberButton) => {
+    numberButton.addEventListener("click", function () {
+        if (num1 && operator) {
+            num2Arr.push(numberButton.innerText);
+        }
+    });
+});
+
+equalsButton.addEventListener("click", function () {
+    if (num1Arr && num1 && operator) {
+        num2 = parseInt(num2Arr.join(""));
+        console.log(operate(operator, num1, num2));
+    }
+});
