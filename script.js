ad8ceaaedba7744110d107a861b72780c77cf171
Danny Moulier <mr.moulier@gmail.com>
2023-09-20 13:30:46 +0300

    added keyboard support and code structure improved

--- script.js
+++ script.js
@@ -18,9 +18,6 @@ function modulo(num1, num2) {
     return num1 % num2;
 }
 
-let num1;
-let num2;
-let operator;
 
 function operate(operator, num1, num2) {
     if (operator === "+") {
@@ -45,100 +42,39 @@ function evaluate() {
     numArr = [];
 }
 
-const numberButtons = document.querySelectorAll(".number");
-const operatorButtons = document.querySelectorAll(".operator");
-const equalsButton = document.querySelector("#equals");
-const display = document.querySelector("#display");
-const clearButton = document.querySelector("#ac");
-const deleteButton = document.querySelector("#del");
-const dotButton = document.querySelector("#dot");
-
-let numArr = [];
-display.innerText = 0;
-equalsClicked = false
-
-const numberKeys = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
-const operatorKeys = ['%', '+', '-', '/', '*']
-const evaluationKeys = ['=', 'Enter']
-const clearKey = ['c']
-const deleteKey = ['Backspace']
-let dotKey = ['.']
-
-numberButtons.forEach((numberButton) => {
-    numberButton.addEventListener("click", function () {
-        if (equalsClicked) {
-            num1 = undefined;
-            equalsClicked = false;
-        }
-        if (!operator) {
-            numArr.push(numberButton.innerText);
-            display.innerText = numArr.join("");
-        }
-        if (num1 >= 0 && operator) {
-            numArr.push(numberButton.innerText);
-            display.innerText = numArr.join("");
-        }
-    });
-});
-
-
-window.addEventListener('keydown', function (e) {
-    if (numberKeys.includes(e.key)) {
-        if (equalsClicked) {
-            num1 = undefined;
-            equalsClicked = false;
-        }
-        if (!operator) {
-            numArr.push(e.key);
-            display.innerText = numArr.join("");
-        }
-        if (num1 >= 0 && operator) {
-            numArr.push(e.key);
-            display.innerText = numArr.join("");
-        }
-    }
-});
-
-
-operatorButtons.forEach((operatorButton) => {
-    operatorButton.addEventListener("click", function () {
-        if (operator) {
-            evaluate();
-            dotButton.disabled = false;
-            dotKey = ['.']
-        } else if (numArr || num1 >= 0) {
-            if (!num1) {
-                num1 = Number(numArr.join(""));
-            }
-        }
-        operator = operatorButton.innerText;
-        numArr = [];
-        dotButton.disabled = false;
-        dotKey = ['.']
-        equalsClicked = false;
-    });
-});
-
-window.addEventListener('keydown', function (e) {
-    if (operatorKeys.includes(e.key)) {
-        if (operator) {
-            evaluate();
-            dotButton.disabled = false;
-            dotKey = ['.']
-        } else if (numArr || num1 >= 0) {
-            if (!num1) {
-                num1 = Number(numArr.join(""));
-            }
-        }
-        operator = e.key;
-        numArr = [];
-        dotButton.disabled = false;
-        dotKey = ['.']
+function choseNumber(arg) {
+    if (equalsClicked) {
+        num1 = undefined;
         equalsClicked = false;
     }
-})
+    if (!operator) {
+        numArr.push(arg);
+        display.innerText = numArr.join("");
+    }
+    if (num1 >= 0 && operator) {
+        numArr.push(arg);
+        display.innerText = numArr.join("");
+    }
+}
 
-equalsButton.addEventListener("click", function () {
+function choseOperator(arg) {
+    if (operator) {
+        evaluate();
+        dotButton.disabled = false;
+        dotKey = ["."];
+    } else if (numArr || num1 >= 0) {
+        if (!num1) {
+            num1 = Number(numArr.join(""));
+        }
+    }
+    operator = arg;
+    numArr = [];
+    dotButton.disabled = false;
+    dotKey = ["."];
+    equalsClicked = false;
+}
+
+function equals() {
     if (numArr && num1 >= 0 && operator) {
         evaluate();
         operator = undefined;
@@ -152,82 +88,117 @@ equalsButton.addEventListener("click", function () {
         numArr = [];
         display.innerText = "";
         dotButton.disabled = false;
-        dotKey = ['.']
-        display.innerText = 'Math Err';
-        equalsClicked = false
+        dotKey = ["."];
+        display.innerText = "Math Err";
+        equalsClicked = false;
     }
-});
+}
 
-window.addEventListener('keydown', function (e) {
-    if (evaluationKeys.includes(e.key)) {
-        if (numArr && num1 >= 0 && operator) {
-            evaluate();
-            operator = undefined;
-        }
-        dotButton.disabled = false;
-        dotKey = ['.']
-        equalsClicked = true;
-        if (num1 === Infinity) {
-            num1 = undefined;
-            num2 = undefined;
-            operator = undefined;
-            numArr = [];
-            display.innerText = "";
-            dotButton.disabled = false;
-            dotKey = ['.']
-            display.innerText = 'Math Err';
-            equalsClicked = false
-        }
-    }
-})
-
-clearButton.addEventListener("click", function () {
+function clearAll() {
     num1 = undefined;
     num2 = undefined;
     operator = undefined;
     numArr = [];
     display.innerText = 0;
     dotButton.disabled = false;
-    equalsClicked = false
-});
+    equalsClicked = false;
+}
 
-window.addEventListener('keydown', function (e) {
-    if (clearKey.includes(e.key)) {
-        num1 = undefined;
-        num2 = undefined;
-        operator = undefined;
-        numArr = [];
-        display.innerText = 0;
-        dotButton.disabled = false;
-        dotKey = ['.']
-        equalsClicked = false
-    }
-})
-
-deleteButton.addEventListener("click", function () {
+function deleteChar() {
     numArr.pop();
     display.innerText = numArr.join("");
-});
+}
 
-window.addEventListener('keydown', function (e) {
-    if (deleteKey.includes(e.key)) {
-        numArr.pop();
-        display.innerText = numArr.join("");
-    }
-})
-
-dotButton.addEventListener("click", function () {
+function addDecimalPoint() {
     numArr.push(dotButton.innerText);
     display.innerText = numArr.join("");
     dotButton.disabled = true;
-    dotKey = []
+    dotKey = [];
+}
+
+let num1;
+let num2;
+let operator;
+
+const numberButtons = document.querySelectorAll(".number");
+const operatorButtons = document.querySelectorAll(".operator");
+const equalsButton = document.querySelector("#equals");
+const display = document.querySelector("#display");
+const clearButton = document.querySelector("#ac");
+const deleteButton = document.querySelector("#del");
+const dotButton = document.querySelector("#dot");
+
+let numArr = [];
+display.innerText = 0;
+equalsClicked = false;
+
+const numberKeys = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+const operatorKeys = ["%", "+", "-", "/", "*"];
+const evaluationKeys = ["=", "Enter"];
+const clearKey = ["c"];
+const deleteKey = ["Backspace"];
+let dotKey = ["."];
+
+numberButtons.forEach((numberButton) => {
+    numberButton.addEventListener("click", function () {
+        choseNumber(numberButton.innerText);
+    });
 });
 
-window.addEventListener('keydown', function (e) {
-    if (dotKey.includes(e.key)) {
-        numArr.push(dotButton.innerText);
-        display.innerText = numArr.join("");
-        dotButton.disabled = true;
-        dotKey = []
+operatorButtons.forEach((operatorButton) => {
+    operatorButton.addEventListener("click", function () {
+        choseOperator(operatorButton.innerText);
+    });
+});
+
+// MOUSE CONTROLS
+
+equalsButton.addEventListener("click", function () {
+    equals();
+});
+
+clearButton.addEventListener("click", function () {
+    clearAll();
+});
+
+deleteButton.addEventListener("click", function () {
+    deleteChar();
+});
+
+dotButton.addEventListener("click", function () {
+    addDecimalPoint();
+});
+
+window.addEventListener("keydown", function (e) {
+    if (numberKeys.includes(e.key)) {
+        choseNumber(e.key);
     }
-})
+});
+
+// KEYBOARD CONTROLS
+
+window.addEventListener("keydown", function (e) {
+    if (operatorKeys.includes(e.key)) {
+        choseOperator(e.key);
+    }
+});
+window.addEventListener("keydown", function (e) {
+    if (evaluationKeys.includes(e.key)) {
+        equals();
+    }
+});
+window.addEventListener("keydown", function (e) {
+    if (clearKey.includes(e.key)) {
+        clearAll();
+    }
+});
+window.addEventListener("keydown", function (e) {
+    if (deleteKey.includes(e.key)) {
+        deleteChar();
+    }
+});
+window.addEventListener("keydown", function (e) {
+    if (dotKey.includes(e.key)) {
+        addDecimalPoint();
+    }
+});
